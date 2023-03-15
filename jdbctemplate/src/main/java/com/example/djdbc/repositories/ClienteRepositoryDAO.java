5a5563a2c03cc1595e92d8e7e6bf4b61bdafbf29
Renan Mendonça <57994919+RenanKlinsmann@users.noreply.github.com>
2023-03-14 21:18:47 -0300

    fix(parte-dois): aula jdbc template

---
+++ jdbctemplate/src/main/java/com/example/djdbc/repositories/ClienteRepositoryDAO.java
@@ -0,0 +1,39 @@
+package com.example.djdbc.repositories;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import com.example.djdbc.entities.Cliente;
+
+@Repository
+public class ClienteRepositoryDAO {
+	
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	String querySelectUser = "Select * from CLIENTE";
+	
+	public class ClienteRowMapper implements RowMapper<Cliente>{
+		@Override
+		public Cliente mapRow(ResultSet rs, int rowNum) throws SQLException {
+			Cliente cliente = new Cliente();
+			cliente.setId(rs.getInt("ID"));
+			cliente.setUsername(rs.getString("USERNAME"));
+			cliente.setEmail(rs.getString("EMAIL"));
+			cliente.setPassword(rs.getString("PASSWORD"));
+			return cliente;
+		}	
+	}
+	
+	public List<Cliente> getAllClientes(){
+		return jdbcTemplate.query(querySelectUser, new ClienteRowMapper());
+	}
+
+}
